e2fc4fac3388ab457670a573ee971b9ff95da636
jannick vandaele <jannickvandaele@gmail.com>
2020-03-30 19:58:43 +0200

    Updates

---
+++ precache-manifest.803fe4022a70f7772d81d8e8a5fd2a8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8851dc5e7bcd09d79da07af53b0c26",
+    "url": "/Hosting-Jannick/index.html"
+  },
+  {
+    "revision": "2f3e1ee83db96d1f9eb1",
+    "url": "/Hosting-Jannick/static/css/2.d7be0ab4.chunk.css"
+  },
+  {
+    "revision": "2f3e1ee83db96d1f9eb1",
+    "url": "/Hosting-Jannick/static/js/2.4280f181.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Hosting-Jannick/static/js/2.4280f181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26863da670f1e284ba59",
+    "url": "/Hosting-Jannick/static/js/main.bde8e531.chunk.js"
+  },
+  {
+    "revision": "f00323569051ae07320d",
+    "url": "/Hosting-Jannick/static/js/runtime-main.162fe616.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Hosting-Jannick/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Hosting-Jannick/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Hosting-Jannick/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Hosting-Jannick/static/media/slick.f97e3bbf.svg"
+  }
+]);
