dd4e50000d68d3c8569f78ad51f60e1f8f1bffda
jannick vandaele <jannickvandaele@gmail.com>
2020-03-30 21:10:38 +0200

    Update Homepage

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     };
     return (
       <div>
-        <h2>Jannick's Website</h2>
+        <h2>Jannick's Website Updated</h2>
         <Slider {...settings}>
           <div>
             <img src="https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"/>
